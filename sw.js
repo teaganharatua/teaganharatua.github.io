26d1ea928e52e37988538d3b94ef8b479c8f1e6a
Team Fobuma <teagan.haratua@gmail.com>
2019-09-05 15:43:58 +0700

    new stuffer

--- sw.js
+++ sw.js
@@ -6,11 +6,13 @@ const filesToCache = [
   'index.html',
   'main.js',
   'sw.js',
-  
+  '404.html',
+  'offline.html',
 ];
 
-const staticCacheName = 'pages-cache-v1';
+const staticCacheName = 'pages-cache-v2';
 
+//cache on SW start
 self.addEventListener('install', event => {
   console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
@@ -21,6 +23,26 @@ self.addEventListener('install', event => {
   );
 });
 
+//initialize new service worker
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+//fetch for fetching the cache, and also redirect on offline or wrong link
 self.addEventListener('fetch', event => {
   console.log('Fetch event for ', event.request.url);
   event.respondWith(
@@ -32,12 +54,19 @@ self.addEventListener('fetch', event => {
       }
       console.log('Network request for ', event.request.url);
       return fetch(event.request)
-
-
+      .then(response => {
+        if (response.status === 404) {
+          return caches.match('404.html');
+        }
+        return caches.open(staticCacheName)
+        .then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
     }).catch(error => {
-
-      
-
+      console.log('Error, ', error);
+      return caches.match('offline.html');
     })
   );
 });

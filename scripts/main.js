9b23c81354b9592d1e3c2a3ed1fb982695a9eee2
Team Fobuma <teagan.haratua@gmail.com>
2019-09-13 14:23:56 +0700

    commit massage

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-//addBtn.style.display = 'none';
+addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
